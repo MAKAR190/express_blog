527a4de4a2dc50a67f66175be544014e2e45cce0
MAKAR190 <lutskyimakar@gmail.com>
2022-07-24 12:29:14 +0300

    pushed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -38,92 +38,35 @@ exports.getUser = async (req, res) => {
     res.status(500).send(error);
   }
 };
-(exports.postUser = auth),
-  async (req, res) => {
-    try {
-      const following = req.user.following.includes(req.params.userId);
-      const followUser = await User.findById(req.params.userId);
-      if (following) {
-        req.user.following.pull(req.params.userId);
-        followUser.followers.pull(req.user._id);
-      } else {
-        req.user.following.addToSet(req.params.userId);
-        followUser.followers.addToSet(req.user._id);
-      }
-      await req.user.save();
-      await followUser.save();
-      res.json({
-        followUser: followUser,
-      });
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  };
-(exports.updateUser = schemaValidate(userValidate.update)),
-  auth,
-  async (req, res) => {
-    try {
-      if (req.user._id !== req.params.userId) {
-        res.status(403).json({ message: "Error 403" });
-        return;
-      }
-      const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
-        new: true,
-      });
-      if (!user) {
-        res.status(404).json({ message: "Not found" });
-        return;
-      }
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  };
-(exports.deleteUser = auth),
-  async (req, res) => {
-    try {
-      if (req.user._id !== req.params.userId) {
-        res.status(403).json({ message: "Error 403" });
-        return;
-      }
-      const user = await User.findByIdAndDelete(req.params.userId);
-      if (!(await user.findById(req.params.userId))) {
-        res.status(404).json({ message: "Not found" });
-        return;
-      }
-      res.status(200).json({ message: "User successfully deleted!" });
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  };
-
-exports.getUser = async (req, res) => {
+exports.postUser = async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId)
-      .populate({
-        path: "readingList",
-        populate: [
-          {
-            path: "author",
-          },
-          {
-            path: "tags",
-          },
-        ],
-      })
-      .populate("likedPosts")
-      .populate("likesComments")
-      .populate({
-        path: "posts",
-        populate: [
-          {
-            path: "author",
-          },
-          {
-            path: "tags",
-          },
-        ],
-      });
+    const following = req.user.following.includes(req.params.userId);
+    const followUser = await User.findById(req.params.userId);
+    if (following) {
+      req.user.following.pull(req.params.userId);
+      followUser.followers.pull(req.user._id);
+    } else {
+      req.user.following.addToSet(req.params.userId);
+      followUser.followers.addToSet(req.user._id);
+    }
+    await req.user.save();
+    await followUser.save();
+    res.json({
+      followUser: followUser,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+exports.updateUser = async (req, res) => {
+  try {
+    if (req.user._id !== req.params.userId) {
+      res.status(403).json({ message: "Error 403" });
+      return;
+    }
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+      new: true,
+    });
     if (!user) {
       res.status(404).json({ message: "Not found" });
       return;
@@ -133,7 +76,22 @@ exports.getUser = async (req, res) => {
     res.status(500).send(error);
   }
 };
-
+exports.deleteUser = async (req, res) => {
+  try {
+    if (req.user._id !== req.params.userId) {
+      res.status(403).json({ message: "Error 403" });
+      return;
+    }
+    const user = await User.findByIdAndDelete(req.params.userId);
+    if (!(await user.findById(req.params.userId))) {
+      res.status(404).json({ message: "Not found" });
+      return;
+    }
+    res.status(200).json({ message: "User successfully deleted!" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 exports.searchUsers = async (req, res) => {
   try {
     let { search = "", perPage = 10, page = 1, sortBy, sortOrder } = req.query;
